215a33bd94e07aac9c799de9e533b8abf37a283c
glowredman <35727266+glowredman@users.noreply.github.com>
2020-05-07 01:38:36 +0200

    started presets

--- src/main/java/glowredman/modularmaterials/object/Type.java
+++ src/main/java/glowredman/modularmaterials/object/Type.java
@@ -2,15 +2,25 @@ package glowredman.modularmaterials.object;
 
 public class Type {
 	
-	public boolean disabled;
 	public String category;
+	public boolean disabled;
 	public String oreDictPrefix;
+	public String state;
 	public String syntax;
 	public String unitValue;
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
 	
 	public boolean isDisabled() {
 		return disabled;
 	}
+	
 	public void setDisabled(boolean disabled) {
 		this.disabled = disabled;
 	}
@@ -18,24 +28,31 @@ public class Type {
 	public String getUnitValue() {
 		return unitValue;
 	}
+	
 	public void setUnitValue(String unitValue) {
 		this.unitValue = unitValue;
 	}
+	
 	public String getCategory() {
 		return category;
 	}
+	
 	public String getOreDictPrefix() {
 		return oreDictPrefix;
 	}
+	
 	public String getSyntax() {
 		return syntax;
 	}
+	
 	public void setCategory(String category) {
 		this.category = category;
 	}
+	
 	public void setOreDictPrefix(String oreDictPrefix) {
 		this.oreDictPrefix = oreDictPrefix;
 	}
+	
 	public void setSyntax(String syntax) {
 		this.syntax = syntax;
 	}
