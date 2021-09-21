5f02c30681065102684e0582014141c473455933
glowredman <fredcraft00@gmail.com>
2021-09-21 22:42:13 +0200

    Add creative tabs

---
+++ src/main/java/glowredman/modularmaterials/MM_CreativeTabs.java
@@ -0,0 +1,34 @@
+package glowredman.modularmaterials;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+
+public class MM_CreativeTabs {
+	
+	public static final CreativeModeTab TAB_BLOCKS = new CreativeModeTab(MM_Reference.MODID + ".blocks") {
+		
+		@Override
+		public ItemStack makeIcon() {
+			return new ItemStack(Blocks.IRON_BLOCK);
+		}
+	};
+	
+	public static final CreativeModeTab TAB_ITEMS = new CreativeModeTab(MM_Reference.MODID + ".items") {
+		
+		@Override
+		public ItemStack makeIcon() {
+			return new ItemStack(Items.IRON_INGOT);
+		}
+	};
+	
+	public static final CreativeModeTab TAB_ORES = new CreativeModeTab(MM_Reference.MODID + ".ores") {
+		
+		@Override
+		public ItemStack makeIcon() {
+			return new ItemStack(Blocks.IRON_ORE);
+		}
+	};
+
+}
