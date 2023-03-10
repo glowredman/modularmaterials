c8b0538e6428b90555c7fb1978957ad21fcc3596
glowredman <35727266+glowredman@users.noreply.github.com>
2023-03-10 14:16:43 +0100

    Attempt to fix worldgen

--- src/main/java/glowredman/modularmaterials/worldgen/FeatureHandler.java
+++ src/main/java/glowredman/modularmaterials/worldgen/FeatureHandler.java
@@ -13,14 +13,11 @@ import glowredman.modularmaterials.MM_Reference;
 import glowredman.modularmaterials.ModularMaterials;
 import glowredman.modularmaterials.data.object.MM_OreVein;
 import net.minecraft.core.Holder;
-import net.minecraft.data.BuiltinRegistries;
-import net.minecraft.data.worldgen.features.FeatureUtils;
-import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.GenerationStep.Decoration;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
@@ -28,7 +25,9 @@ import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ModifiableBiomeInfo.BiomeInfo.Builder;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries.Keys;
+import net.minecraftforge.registries.ForgeRegistry;
 import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.registries.RegistryManager;
 import net.minecraftforge.registries.RegistryObject;
 
 public class FeatureHandler {
@@ -37,8 +36,6 @@ public class FeatureHandler {
 	public static int totalWeight;
 	
 	public static final Feature<NoneFeatureConfiguration> OREVEIN = new FeatureVeinLayer();
-	public static final Holder<ConfiguredFeature<NoneFeatureConfiguration, ?>> CONFIGURED_OREVEIN = FeatureUtils.register(MM_Reference.MODID + ":configured_orevein", OREVEIN);
-	public static final Holder<PlacedFeature> PLACED_OREVEIN = PlacementUtils.register(MM_Reference.MODID + ":placed_orevein", CONFIGURED_OREVEIN);
 	
 	public static void initOresForBlocksList() {
 		blocksWithVariants = MM_Reference.ORES.rowKeySet();
@@ -53,13 +50,12 @@ public class FeatureHandler {
 	
 	@SubscribeEvent
 	public void registerBiomeModifiers(RegisterEvent event) {
-		if(!event.getRegistryKey().equals(Keys.BIOME_MODIFIER_SERIALIZERS)) {
-			return;
+		if(event.getRegistryKey().equals(Keys.BIOME_MODIFIER_SERIALIZERS)) {
+			event.getForgeRegistry().register(new ResourceLocation(MM_Reference.MODID, "orevein_modifier"), OreVeinBiomeModifier.makeCodec());
 		}
-		event.getForgeRegistry().register(new ResourceLocation(MM_Reference.MODID, "orevein_modifier"), OreVeinBiomeModifier.makeCodec());
 	}
 	
-	public record OreVeinBiomeModifier(boolean enabled) implements BiomeModifier {
+	public record OreVeinBiomeModifier(boolean enabled, Holder<PlacedFeature> feature) implements BiomeModifier {
 		
 		private static final RegistryObject<Codec<? extends BiomeModifier>> SERIALIZER = RegistryObject.create(new ResourceLocation(MM_Reference.MODID, "orevein_modifier"), Keys.BIOME_MODIFIER_SERIALIZERS, MM_Reference.MODID);
 		private static Set<DataResult<JsonElement>> featuresToRemove;
@@ -67,7 +63,7 @@ public class FeatureHandler {
 		@Override
 		public void modify(Holder<Biome> biome, Phase phase, Builder builder) {
 			if(phase == Phase.ADD && enabled) {
-				builder.getGenerationSettings().addFeature(Decoration.UNDERGROUND_ORES, PLACED_OREVEIN);
+				builder.getGenerationSettings().addFeature(Decoration.UNDERGROUND_ORES, feature);
 			}
 			else if(phase == Phase.REMOVE && enabled) {
 				builder.getGenerationSettings().getFeatures(Decoration.UNDERGROUND_ORES).removeIf(OreVeinBiomeModifier::checkFeature);
@@ -82,8 +78,9 @@ public class FeatureHandler {
 		private static boolean checkFeature(Holder<PlacedFeature> feature) {
 			if(featuresToRemove == null) {
 				featuresToRemove = new HashSet<>();
+				ForgeRegistry<PlacedFeature> registry = RegistryManager.VANILLA.getRegistry(Registries.PLACED_FEATURE);
 				for(String s : MM_Reference.CONFIG.removeOres) {
-					PlacedFeature p = BuiltinRegistries.PLACED_FEATURE.get(new ResourceLocation(s));
+					PlacedFeature p = registry.getValue(new ResourceLocation(s));
 					if(p == null) {
 						ModularMaterials.warn(s + " is not a valid PlacedFeature!");
 						continue;
@@ -95,7 +92,7 @@ public class FeatureHandler {
 		}
 		
 		private static Codec<OreVeinBiomeModifier> makeCodec() {
-			return RecordCodecBuilder.create(builder -> builder.group(Codec.BOOL.fieldOf("enabled").forGetter(OreVeinBiomeModifier::enabled)).apply(builder, OreVeinBiomeModifier::new));
+			return RecordCodecBuilder.create(builder -> builder.group(Codec.BOOL.fieldOf("enabled").forGetter(OreVeinBiomeModifier::enabled), PlacedFeature.CODEC.fieldOf("features").forGetter(OreVeinBiomeModifier::feature)).apply(builder, OreVeinBiomeModifier::new));
 		}
 		
 	}
